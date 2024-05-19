de5dab094683e0ddbca4722efa51d183046b3311
yu7400ki <yu7400ki@gmail.com>
2024-05-20 03:38:46 +0900

    save to pdf

--- collect-cli/src/main.rs
+++ collect-cli/src/main.rs
@@ -1,3 +1,136 @@
-fn main() {
-    println!("Hello, world!");
+mod pdf;
+
+use std::time::Duration;
+
+use clap::Parser;
+use collect::{
+    iniad::{login_google, login_moocs, Credentials},
+    moocs::Course,
+};
+use dialoguer::{Input, Password, Select};
+use indicatif::ProgressBar;
+use reqwest::Client;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(long)]
+    path: Option<String>,
+    #[arg(long)]
+    year: Option<u32>,
+    #[arg(long)]
+    no_image_embedding: bool,
+}
+
+fn create_client() -> reqwest::Result<Client> {
+    let client = Client::builder()
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0")
+        .cookie_store(true)
+        .build()?;
+    Ok(client)
+}
+
+struct Spinner {
+    spinner: ProgressBar,
+}
+
+impl Spinner {
+    fn new() -> Self {
+        let spinner = ProgressBar::new_spinner();
+        spinner.enable_steady_tick(Duration::from_millis(100));
+        Self { spinner }
+    }
+
+    fn set_message(&self, msg: &'static str) {
+        self.spinner.set_message(msg);
+    }
+}
+
+impl Drop for Spinner {
+    fn drop(&mut self) {
+        self.spinner.finish_and_clear();
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    let client = create_client()?;
+    let credentials = {
+        let username: String = Input::new().with_prompt("ユーザ名").interact_text()?;
+        let password: String = Password::new().with_prompt("パスワード").interact()?;
+        Credentials { username, password }
+    };
+
+    let logged_in = {
+        let s = Spinner::new();
+        s.set_message("ログイン中...");
+        let mut logged_in = login_moocs(&client, &credentials).await?;
+        logged_in |= login_google(&client, &credentials).await?;
+        logged_in
+    };
+    if !logged_in {
+        eprintln!("ログインに失敗しました\nユーザ名とパスワードを確認してください");
+        std::process::exit(1);
+    }
+
+    let courses = {
+        let s = Spinner::new();
+        s.set_message("コースを取得中...");
+        Course::list(&client, args.year).await?
+    };
+    let course_selection = Select::new()
+        .with_prompt("コースを選択")
+        .items(&courses)
+        .default(0)
+        .max_length(10)
+        .interact()?;
+    let course = &courses[course_selection];
+
+    let lectures = {
+        let s = Spinner::new();
+        s.set_message("授業を取得中...");
+        course.lectures(&client).await?
+    };
+    let lecture_selection = Select::new()
+        .with_prompt("授業を選択")
+        .items(&lectures)
+        .default(0)
+        .max_length(10)
+        .interact()?;
+    let lecture = &lectures[lecture_selection];
+
+    let pages = {
+        let s = Spinner::new();
+        s.set_message("ページを取得中...");
+        let pages = lecture.pages(&client).await?;
+        let mut pages_has_slide = vec![];
+        for page in pages {
+            let has_slide = page.has_slide(&client).await?;
+            if has_slide {
+                pages_has_slide.push(page);
+            }
+        }
+        pages_has_slide
+    };
+    let page_selection = Select::new()
+        .with_prompt("ページを選択")
+        .items(&pages)
+        .default(0)
+        .max_length(10)
+        .interact()?;
+    let page = &pages[page_selection];
+
+    {
+        let s = Spinner::new();
+        s.set_message("保存中...");
+        let slides = page.slides(&client).await?;
+        let mut slide = slides[0].clone();
+        slide.embed_image(&client).await?;
+        let mut pdf = pdf::convert(&slide)?;
+        pdf.save("output.pdf")?;
+        println!("output.pdf に保存しました");
+    }
+
+    Ok(())
 }
---
+++ collect-cli/src/pdf.rs
@@ -0,0 +1,61 @@
+use collect::moocs::Slide;
+use lopdf::dictionary;
+use lopdf::{Document, Object};
+use rayon::prelude::*;
+use svg2pdf::{convert_str, Options};
+
+pub fn convert(slide: &Slide) -> anyhow::Result<Document> {
+    let documents = slide
+        .content
+        .par_iter()
+        .map(|src| {
+            let options = Options::default();
+            convert_str(src, options)
+                .map_err(anyhow::Error::from)
+                .and_then(|data| Document::load_mem(&data).map_err(anyhow::Error::from))
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?;
+
+    let pdf = merge(documents)?;
+
+    Ok(pdf)
+}
+
+fn merge(documents: Vec<Document>) -> anyhow::Result<Document> {
+    let mut merged = Document::with_version("1.5");
+    let mut document_pages = vec![];
+
+    for mut document in documents {
+        document.renumber_objects_with(merged.max_id + 1);
+        merged.max_id = document.max_id;
+
+        for (_, object_id) in document.get_pages() {
+            let reference = Object::Reference(object_id);
+            document_pages.push(reference);
+        }
+
+        merged.objects.extend(document.objects);
+    }
+
+    let count = document_pages.len() as u32;
+    let pages = lopdf::dictionary! {
+        "Type" => "Pages",
+        "Kids" => document_pages,
+        "Count" => count,
+    };
+    let pages_id = merged.new_object_id();
+    merged.objects.insert(pages_id, Object::Dictionary(pages));
+
+    let catalog_id = merged.new_object_id();
+    let catalog = lopdf::dictionary! {
+        "Type" => "Catalog",
+        "Pages" => pages_id,
+    };
+
+    merged
+        .objects
+        .insert(catalog_id, Object::Dictionary(catalog));
+    merged.trailer.set("Root", catalog_id);
+
+    Ok(merged)
+}
