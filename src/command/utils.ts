d1e297dd6a109bc152726cd96a0d11aec75d76e3
yu7400ki <yu7400ki@gmail.com>
2025-03-03 03:47:25 +0900

    fix create command

--- src/command/utils.ts
+++ src/command/utils.ts
@@ -1,7 +1,17 @@
-import { type InvokeArgs, invoke } from "@tauri-apps/api/core";
+import {
+  type InvokeArgs,
+  type InvokeOptions,
+  invoke,
+} from "@tauri-apps/api/core";
 
-export function createCommand<Args extends InvokeArgs, Out>(command: string) {
-  return async (input: Args) => {
-    return await invoke<Out>(command, input);
-  };
+type Command<Args extends InvokeArgs | undefined, Out> = Args extends undefined
+  ? (args?: Args, options?: InvokeOptions) => Promise<Out>
+  : (args: Args, options?: InvokeOptions) => Promise<Out>;
+
+export function createCommand<Args extends InvokeArgs | undefined, Out>(
+  command: string,
+): Command<Args, Out> {
+  return (async (input?: Args, options?: InvokeOptions) => {
+    return await invoke<Out>(command, input, options);
+  }) as Command<Args, Out>;
 }
