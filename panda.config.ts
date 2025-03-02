6f4ea77ac4381fe616be4cba5a0e1d01ae6bbfbd
yu7400ki <yu7400ki@gmail.com>
2025-03-03 01:21:56 +0900

    theme

--- panda.config.ts
+++ panda.config.ts
@@ -11,4 +11,24 @@ export default defineConfig({
   include: ["./src/**/*.{js,jsx,ts,tsx,vue}"],
   jsxFramework: "react", // or 'solid' or 'vue'
   outdir: "styled-system",
+
+  theme: {
+    extend: {
+      tokens: {
+        fonts: {
+          japanese: { value: "'Hiragino Sans', 'BIZ UDPGothic', 'sans-serif'" },
+          latin: {
+            value:
+              "Inter, Roboto, 'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif",
+          },
+        },
+      },
+    },
+  },
+
+  globalCss: {
+    body: {
+      fontFamily: "japanese",
+    },
+  },
 });
