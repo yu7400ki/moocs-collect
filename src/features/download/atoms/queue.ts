c46bec9090b92ebb4f6f350ec921d0e6ff3ed2bb
yu7400ki <yu7400ki@gmail.com>
2025-03-08 22:47:34 +0900

    job queue

---
+++ src/features/download/atoms/queue.ts
@@ -0,0 +1,28 @@
+import type { Page } from "@/features/course/schemas/page";
+import { atomWithProxy } from "jotai-valtio";
+import PQueue from "p-queue";
+import { proxySet } from "valtio/utils";
+import { proxy } from "valtio/vanilla";
+
+const queue = new PQueue({ concurrency: 10 });
+
+const queueState = proxy({
+  pending: proxySet(),
+  running: proxySet(),
+  completed: proxySet(),
+});
+
+export async function enqueue(page: Page) {
+  queueState.pending.add(page);
+  await queue.add(async () => {
+    queueState.pending.delete(page);
+    queueState.running.add(page);
+    console.log("Start Processing", page);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("End Processing", page);
+    queueState.running.delete(page);
+    queueState.completed.add(page);
+  });
+}
+
+export const queueAtom = atomWithProxy(queueState, { sync: true });
