48c73ac51dee929b6c6bd047b3739c95fcfb4f0e
yu7400ki <yu7400ki@gmail.com>
2025-03-12 05:00:43 +0900

    init settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Providers } from "./components/providers";
+import { store } from "./components/providers/jotai";
+import { getSettings } from "./features/settings/services/settings";
 import "./index.css";
 import "unfonts.css";
+import { settingsAtom } from "./features/settings/atoms/settings";
 
-const rootElement = document.getElementById("root");
-if (rootElement && !rootElement.innerHTML) {
-  const root = createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <Providers />
-    </StrictMode>,
-  );
+async function initSettings() {
+  const settings = await getSettings();
+  await store.set(settingsAtom, settings);
 }
+
+initSettings().then(() => {
+  const rootElement = document.getElementById("root");
+  if (rootElement && !rootElement.innerHTML) {
+    const root = createRoot(rootElement);
+    root.render(
+      <StrictMode>
+        <Providers />
+      </StrictMode>,
+    );
+  }
+});
